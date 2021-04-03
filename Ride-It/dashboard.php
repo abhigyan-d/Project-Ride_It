706320a82361203e9871dd014a28642ff9cea5fc
abhigyan-d <81702047+abhigyan-d@users.noreply.github.com>
2021-04-04 03:54:53 +0530

    Add files via upload

---
+++ Ride-It/dashboard.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php
+    include('session.php');
+?>
+
+<html lang="en">
+    <head>
+        <title>
+            Dashboard
+        </title>
+    </head>
+    <body>
+        <h1>Hemlo!</h1>
+        <p><a href="logout.php">LogOut</p>
+    </body>
+</html>
