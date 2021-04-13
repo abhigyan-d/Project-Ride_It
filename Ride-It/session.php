c8ab4c31ad214eb3e8c92e85009d18fd42962da5
abhigyan-d <81702047+abhigyan-d@users.noreply.github.com>
2021-04-13 15:42:00 +0530

    Delete Ride-It directory

--- Ride-It/session.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include('config.php');
-    session_start();
-    $user_check=$_SESSION['login_user'];
-    $ses_sql=mysqli_query($db,"select uid from users where uid='$user_check'");
-?>
